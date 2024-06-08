2a94c01c47acfe824d6012dc9b7e98c7a50d0395
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-06-08 23:44:06 +0900

    ログインメソッドを実装

---
+++ ChatBotDiary2024/Views/SignInButton.swift
@@ -0,0 +1,18 @@
+//
+//  SignInButton.swift
+//  ChatBotDiary2024
+//
+//  Created by Gomi Kouki on 2024/06/08.
+//
+
+import SwiftUI
+
+struct SignInButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInButton()
+}
