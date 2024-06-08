9560e7331fa68972a87442d5d432ebdf6e8f2fa3
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-06-08 22:09:53 +0900

    アカウントを新規作成した際に、すでに登録されているメールアドレスだった場合はアラートを出すようにした

---
+++ ChatBotDiary2024/API/FirestoreAPIClient.swift
@@ -0,0 +1,8 @@
+//
+//  FirestoreAPIClient.swift
+//  ChatBotDiary2024
+//
+//  Created by Gomi Kouki on 2024/06/05.
+//
+
+import Foundation
---
+++ ChatBotDiary2024/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  ChatBotDiary2024
+//
+//  Created by Gomi Kouki on 2024/06/05.
+//
+
+import Foundation
