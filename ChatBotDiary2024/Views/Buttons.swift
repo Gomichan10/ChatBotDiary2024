20b5c70818fdb9b4776687529255c4cb1ceeeaf2
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-06-09 23:29:43 +0900

    ログイン機能を実装

--- ChatBotDiary2024/Views/Buttons.swift
+++ ChatBotDiary2024/Views/Buttons.swift
@@ -7,12 +7,41 @@
 
 import SwiftUI
 
-struct Buttons: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+var normalButton: some View {
+    Image(systemName: "arrow.right.circle")
+        .resizable()
+        .frame(width: 70, height: 70)
+        .foregroundColor(.gray)
+        .padding(.top, 50)
+}
+
+var loadingButton: some View {
+    ZStack {
+        Circle()
+            .stroke(Color.gray, style: StrokeStyle(lineWidth: 6))
+            .frame(width: 60, height: 60)
+        ProgressView()
+            .scaleEffect(x: 2, y: 2)
     }
+    .padding(.top, 50)
+}
+
+var completedButton: some View {
+    Image(systemName: "checkmark.circle")
+        .resizable()
+        .frame(width: 70, height: 70)
+        .foregroundColor(.green)
+        .padding(.top, 50)
+}
+
+var errorButton: some View {
+    Image(systemName: "xmark.circle")
+        .resizable()
+        .frame(width: 70, height: 70)
+        .foregroundColor(.red)
+        .padding(.top, 50)
 }
 
 #Preview {
-    Buttons()
+    normalButton
 }
--- ChatBotDiary2024/Views/ContentView.swift
+++ ChatBotDiary2024/Views/ContentView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
-    enum ButtonState {
+    enum LoginButtonState {
         case normal
         case loading
         case completed
         case error
     }
     
-    enum AlertState {
+    enum SigninButtonState {
+        case normal
+        case loading
+        case completed
+        case error
+    }
+    
+    enum AlertSigninState {
         case email
         case pass
         case error
@@ -26,13 +33,18 @@ struct ContentView: View {
         static let baseColor = Color("white_black")
     }
     
-    @State var sheet: Bool = false
-    @State var email: String = ""
-    @State var pass: String = ""
-    @State var buttonState: ButtonState = .normal
-    @State var alertState: AlertState?
+    @State var signinSheet: Bool = false
+    @State var loginSheet: Bool = false
+    @State var loginEmail: String = ""
+    @State var loginPass : String = ""
+    @State var signinEmail: String = ""
+    @State var signinPass: String = ""
+    @State var loginButtonState: LoginButtonState = .normal
+    @State var signinButtonState: SigninButtonState = .normal
+    @State var alertSigninState: AlertSigninState?
     @State var isLarge: Bool = false
-    @State var showAlert: Bool = false
+    @State var showLoginAlert: Bool = false
+    @State var showSigninAlert: Bool = false
     
     
     var body: some View {
@@ -58,6 +70,7 @@ struct ContentView: View {
                 Text("いつもの日記をより楽しく楽に")
                     .foregroundColor(.white)
                 Spacer()
+                googleButton
                 loginButton
                 signInButton
             }
@@ -71,49 +84,110 @@ struct ContentView: View {
 
 extension ContentView {
     
+    //Googleログインボタン
+    var googleButton: some View {
+        Button(
+            action: {},
+            label: {
+                HStack {
+                    Image("GoogleIcon")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .clipShape(Circle())
+                        .padding(.leading, 15)
+                    Spacer()
+                    Text("Googleでログイン")
+                        .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.6))
+                        .bold()
+                        .padding(.trailing, 100)
+                }
+            })
+        .frame(width: 350, height: 55)
+        .background(Color.white)
+        .cornerRadius(.infinity)
+        .shadow(radius: 15)
+    }
+    
     //ログインボタン
     var loginButton: some View {
-        VStack {
-            Button(
-                action: {},
-                label: {
-                    HStack {
-                        Image("GoogleIcon")
-                            .resizable()
-                            .frame(width: 25, height: 25)
-                            .clipShape(Circle())
-                            .padding(.leading, 15)
-                        Spacer()
-                        Text("Googleでログイン")
-                            .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.6))
-                            .bold()
-                            .padding(.trailing, 100)
+        Button(
+            action: {
+                loginSheet.toggle()
+            },
+            label: {
+                HStack {
+                    Image(systemName: "envelope")
+                        .resizable()
+                        .frame(width: 27, height: 20)
+                        .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.65))
+                        .padding(.leading, 15)
+                    Spacer()
+                    Text("メールでログイン")
+                        .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.65))
+                        .bold()
+                        .padding(.trailing, 105)
+                }
+            })
+        .frame(width: 350, height: 55)
+        .background(Color.white)
+        .cornerRadius(.infinity)
+        .shadow(radius: 15)
+        .sheet(isPresented: $loginSheet) {
+            NavigationStack {
+                VStack (spacing: 20){
+                    TextField("メールアドレスを入力", text: $loginEmail)
+                        .padding()
+                        .frame(width: 350, height: 50)
+                        .background(Color(red: 0, green: 0, blue: 0, opacity: 0.07))
+                        .cornerRadius(6)
+                    
+                    SecureField("パスワードを入力", text: $loginPass)
+                        .padding()
+                        .frame(width: 350, height: 50)
+                        .background(Color(red: 0, green: 0, blue: 0, opacity: 0.07))
+                        .cornerRadius(6)
+                    
+                    Button {
+                        switch loginButtonState {
+                        case .normal:
+                            loginButtonState = .loading
+                            UIApplication.shared.endEditing()
+                            UserAPIClient().loginUser(email: loginEmail, password: loginPass) { result in
+                                switch result {
+                                case true:
+                                    loginButtonState = .completed
+                                case false:
+                                    loginButtonState = .error
+                                    showLoginAlert.toggle()
+                                }
+                            }
+                        case .loading:
+                            break
+                        case .completed:
+                            break
+                        case .error:
+                            break
+                        }
+                        
+                    } label: {
+                        switch loginButtonState {
+                        case .normal:
+                            normalButton
+                        case .loading:
+                            loadingButton
+                        case .completed:
+                            completedButton
+                        case .error:
+                            errorButton
+                        }
                     }
-                })
-            .frame(width: 350, height: 55)
-            .background(Color.white)
-            .cornerRadius(.infinity)
-            .shadow(radius: 15)
-            Button(
-                action: {},
-                label: {
-                    HStack {
-                        Image(systemName: "envelope")
-                            .resizable()
-                            .frame(width: 27, height: 20)
-                            .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.65))
-                            .padding(.leading, 15)
-                        Spacer()
-                        Text("メールでログイン")
-                            .foregroundColor(Color(red: 0, green: 0, blue: 0, opacity: 0.65))
-                            .bold()
-                            .padding(.trailing, 105)
-                    }
-                })
-            .frame(width: 350, height: 55)
-            .background(Color.white)
-            .cornerRadius(.infinity)
-            .shadow(radius: 15)
+                }
+                .navigationTitle("ログイン")
+                .alert(isPresented: $showLoginAlert) {
+                    return Alert(title: Text("メールアドレスまたはパスワードが正しくありません"), message: Text("メールアドレスまたはパスワードが正しくありません。もう一度入力し直してください。"), dismissButton: .default(Text("OK"), action: {loginButtonState = .normal}))
+                }
+            }
+            .presentationDetents([.medium])
         }
     }
     
@@ -121,7 +195,7 @@ extension ContentView {
     var signInButton: some View {
         Button(
             action: {
-                sheet.toggle()
+                signinSheet.toggle()
             },
             label: {
                 Text("サインアップ")
@@ -129,56 +203,55 @@ extension ContentView {
                     .bold()
             })
         .padding()
-        .sheet(isPresented: $sheet) {
+        .sheet(isPresented: $signinSheet) {
             NavigationStack {
                 VStack (spacing: 20){
                     
-                    TextField("メールアドレスを入力", text: $email)
+                    TextField("メールアドレスを入力", text: $signinEmail)
                         .padding()
                         .frame(width: 350, height: 50)
                         .background(Color(red: 0, green: 0, blue: 0, opacity: 0.07))
                         .cornerRadius(6)
                     
-                    SecureField("パスワードを入力", text: $pass)
+                    SecureField("パスワードを入力", text: $signinPass)
                         .padding()
                         .frame(width: 350, height: 50)
                         .background(Color(red: 0, green: 0, blue: 0, opacity: 0.07))
                         .cornerRadius(6)
                     
                     Button {
-                        switch buttonState {
+                        switch signinButtonState {
                         case .normal:
-                            buttonState = .loading
+                            signinButtonState = .loading
                             UIApplication.shared.endEditing()
-                            FirestoreAPIClient().searchUser(email: email) { result in
+                            FirestoreAPIClient().searchUser(email: signinEmail) { result in
                                 
                                 switch result {
                                 case true:
-                                    if Validation().isValidEmail(email) {
-                                        if Validation().isValidPassword(pass) {
-                                            UserAPIClient().createUser(email: email, password: pass) { result in
+                                    if Validation().isValidEmail(signinEmail) {
+                                        if Validation().isValidPassword(signinPass) {
+                                            UserAPIClient().createUser(email: signinEmail, password: signinPass) { result in
                                                 switch result {
                                                 case true:
-                                                    print("Success")
-                                                    buttonState = .completed
+                                                    signinButtonState = .completed
                                                 case false:
                                                     print("Failure")
                                                 }
                                             }
                                         } else {
-                                            alertState = .pass
-                                            showAlert = true
-                                            buttonState = .normal
+                                            alertSigninState = .pass
+                                            showSigninAlert = true
+                                            signinButtonState = .normal
                                         }
                                     } else {
-                                        alertState = .email
-                                        showAlert = true
-                                        buttonState = .normal
+                                        alertSigninState = .email
+                                        showSigninAlert = true
+                                        signinButtonState = .normal
                                     }
                                 case false:
-                                    buttonState = .error
-                                    alertState = .error
-                                    showAlert = true
+                                    signinButtonState = .error
+                                    alertSigninState = .error
+                                    showSigninAlert = true
                                 }
                             }
                             
@@ -190,46 +263,27 @@ extension ContentView {
                             break
                         }
                     } label: {
-                        switch buttonState {
+                        switch signinButtonState {
                         case .normal:
-                            Image(systemName: "arrow.right.circle")
-                                .resizable()
-                                .frame(width: 70, height: 70)
-                                .foregroundColor(.gray)
-                                .padding(.top, 50)
+                            normalButton
                         case .loading:
-                            ZStack {
-                                Circle()
-                                    .stroke(Color.gray, style: StrokeStyle(lineWidth: 6))
-                                    .frame(width: 60, height: 60)
-                                ProgressView()
-                                    .scaleEffect(x: 2, y: 2)
-                            }
-                            .padding(.top, 50)
+                            loadingButton
                         case .completed:
-                            Image(systemName: "checkmark.circle")
-                                .resizable()
-                                .frame(width: 70, height: 70)
-                                .foregroundColor(.green)
-                                .padding(.top, 50)
+                            completedButton
                         case .error:
-                            Image(systemName: "xmark.circle")
-                                .resizable()
-                                .frame(width: 70, height: 70)
-                                .foregroundColor(.red)
-                                .padding(.top, 50)
+                            errorButton
                         }
                     }
                 }
                 .navigationTitle("新規アカウント作成")
-                .alert(isPresented: $showAlert) {
-                    switch alertState {
+                .alert(isPresented: $showSigninAlert) {
+                    switch alertSigninState {
                     case .email:
                         return Alert(title: Text("正常なメールアドレスが入力されていません"), message: Text("もう一度メールアドレスを入力し直してください"), dismissButton: .default(Text("OK")))
                     case .pass:
                         return Alert(title: Text("正しいパスワードを入力してください"), message: Text ("最低1文字の小文字または大文字を含み、最低1文字以上の数字を含む10文字以上のパスワードを入力してください"), dismissButton: .default(Text("OK")))
                     case .error:
-                        return Alert(title: Text("すでに登録されているメールアドレスです"), message: Text ("入力されたメールアドレスはすでに登録されています。もう一度入力し直してください。"), dismissButton:.default(Text("OK"), action: { buttonState = .normal }))
+                        return Alert(title: Text("すでに登録されているメールアドレスです"), message: Text ("入力されたメールアドレスはすでに登録されています。もう一度入力し直してください。"), dismissButton:.default(Text("OK"), action: { signinButtonState = .normal }))
                     case .none:
                         return Alert(title: Text("エラー"), message: Text("エラーが発生しました"), dismissButton: .default(Text("OK")))
                     }
