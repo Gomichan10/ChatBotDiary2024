f1d63b63122f27a26f67a2f1ccbaedb23e230c8f
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-06-09 22:17:07 +0900

    ボタンをファイル分けした

---
+++ ChatBotDiary2024/Views/Buttons.swift
@@ -0,0 +1,18 @@
+//
+//  Buttons.swift
+//  ChatBotDiary2024
+//
+//  Created by Gomi Kouki on 2024/06/09.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Buttons()
+}
