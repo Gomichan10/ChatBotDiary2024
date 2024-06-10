2645cb3fe010aed84a9ed11161a76e1ddb4dacc8
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-06-10 23:34:30 +0900

    TabViewを実装

---
+++ ChatBotDiary2024/Views/CalendarView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  ChatBotDiary2024
+//
+//  Created by Gomi Kouki on 2024/06/10.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
---
+++ ChatBotDiary2024/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  
+//
+//  Created by Gomi Kouki on 2024/06/10.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
