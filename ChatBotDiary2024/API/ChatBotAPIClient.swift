58bb6bd87071ddd27babe081662ba7f86d8a32ba
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-07-21 22:29:44 +0900

    カレンダー画面を実装した

--- ChatBotDiary2024/API/ChatBotAPIClient.swift
+++ ChatBotDiary2024/API/ChatBotAPIClient.swift
@@ -3,12 +3,11 @@ import Alamofire
 import FirebaseAuth
 
 class ChatBotAPIClient {
-    private let apiKey = "YOUR_API_KEY_HERE" // 本来のAPIキーを削除または置き換え
+    private let apiKey = "ChatGPT-APIKey"
     private let apiUrl = "https://api.openai.com/v1/chat/completions"
     
     // ChatGPTAPIにメッセージを送受信するメソッド
     func sendMessageChatGPT(messages: [[String: String]], completion: @escaping ([[String: String]]?) -> Void) {
-        var responseMessages: [[String: String]] = []
         
         let headers: HTTPHeaders = [
             "Authorization": "Bearer \(apiKey)",
@@ -20,66 +19,26 @@ class ChatBotAPIClient {
             "messages": messages
         ]
         
-        AF.request(apiUrl, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseData { response in
+        AF.request(apiUrl, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseDecodable(of: ChatGPT.self) { response in
             switch response.result {
-            case .success(let data):
-                do {
-                    if let chatGPTResponse = try? JSONDecoder().decode(ChatGPT.self, from: data) {
-                        print(chatGPTResponse)
-                        if let firstChoice = chatGPTResponse.choices.first {
-                            let content = firstChoice.message.content
-                            // レスポンスメッセージを追加
-                            responseMessages.append(["role": "assistant", "content": content])
-                            
-                            completion(responseMessages)
-                        } else {
-                            completion(nil)
-                        }
-                    } else if let apiError = try? JSONDecoder().decode(APIError.self, from: data) {
-                        print("API Error: \(apiError.error.message)")
-                        completion(nil)
-                    } else {
-                        print("Unexpected data format")
-                        completion(nil)
-                    }
-                } catch {
-                    print("Decoding Error: \(error)")
+            case .success(let gptResponse):
+                if let firstChoice = gptResponse.choices.first {
+                    let content = firstChoice.message.content
+                    var responseMessage: [[String: String]] = [["role": "assistant", "content": content]]
+                    completion(responseMessage)
+                } else {
                     completion(nil)
                 }
             case .failure(let error):
-                print("Error: \(error)")
+                if let data = response.data, let apiError = try? JSONDecoder().decode(APIError.self, from: data) {
+                    print("API Error: \(apiError.error.message)")
+                } else {
+                    print("Error: \(error)")
+                }
                 completion(nil)
             }
         }
     }
 
-    // Difyにメッセージを送るためのメソッド
-    func sendMessageChatbot(message: String, completion: @escaping (Result<ChatResponse, Error>) -> Void) {
-        let apiKey = "YOUR_API_KEY_HERE" // 本来のAPIキーを削除または置き換え
-        let endpoint = "https://api.dify.ai/v1/chat-messages"
-        
-        let headers: HTTPHeaders = [
-            "Authorization": "Bearer \(apiKey)",
-            "Content-Type": "application/json"
-        ]
-        
-        let parameters: [String: Any] = [
-            "message": message
-        ]
-        
-        AF.request(endpoint, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseData { response in
-            switch response.result {
-            case .success(let data):
-                do {
-                    let chatResponse = try JSONDecoder().decode(ChatResponse.self, from: data)
-                    completion(.success(chatResponse))
-                } catch {
-                    completion(.failure(error))
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
 }
 
--- ChatBotDiary2024/Views/ChatView.swift
+++ ChatBotDiary2024/Views/ChatView.swift
@@ -11,6 +11,12 @@ import SwiftUI
 
 struct ChatView: View {
     
+    enum Field: Hashable {
+        case inputField
+    }
+    
+    @FocusState private var focusedField: Field?
+    
     @State private var chatTextField: String = ""
     @State private var conversationID: String = ""
     @State private var chat: [Message] = [Message(id: UUID().uuidString, message: "これから日記を作るよ！どんなことがあったのか教えてね！", isUser: "assistant")]
@@ -42,15 +48,17 @@ struct ChatView: View {
     ]]
     
     var body: some View {
-        messageArea
-            .overlay (
-                navigationArea
-                
-                ,alignment: .top
-            )
-        
-        inputArea
-        
+        NavigationView {
+            VStack (spacing: 0){
+                messageArea
+                    .overlay (
+                        navigationArea
+                        
+                        ,alignment: .top
+                    )
+                inputArea
+            }
+        }
     }
 }
 
@@ -62,58 +70,84 @@ extension ChatView {
     
     var messageArea: some View {
         // Message Area
-        ScrollView {
-            VStack (spacing: 15){
-                ForEach(chat) { chat in
-                    if chat.isUser == "assistant" {
-                        // ChatGPTのメッセージだった場合
-                        HStack (alignment: .top){
-                            Image("ChatBotIcon")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            Text(chat.message)
-                                .padding()
-                                .foregroundColor(.primary)
-                                .background(Color("white_black"))
-                                .cornerRadius(15.0)
-                            
-                            Spacer()
-                        }
-                    } else {
-                        // ユーザのメッセージだった場合
-                        HStack (alignment: .top){
-                            Spacer()
-                            
-                            Text(chat.message)
-                                .padding()
-                                .foregroundColor(Color("white_black"))
-                                .background(.primary)
-                                .cornerRadius(15.0)
-                            Image(systemName: "person.crop.circle")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
+        ScrollViewReader { proxy in
+            ScrollView {
+                VStack (spacing: 20){
+                    ForEach(chat) { chat in
+                        if chat.isUser == "assistant" {
+                            // ChatGPTのメッセージだった場合
+                            HStack (alignment: .top){
+                                Image("ChatBotIcon")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .clipShape(Circle())
+                                Text(chat.message)
+                                    .padding()
+                                    .foregroundColor(.primary)
+                                    .background(Color("white_black"))
+                                    .cornerRadius(15.0)
+                                
+                                Spacer()
+                            }
+                        } else {
+                            // ユーザのメッセージだった場合
+                            HStack (alignment: .top){
+                                Spacer()
+                                
+                                Text(chat.message)
+                                    .padding()
+                                    .foregroundColor(Color("white_black"))
+                                    .background(.primary)
+                                    .cornerRadius(15.0)
+                                Image(systemName: "person.crop.circle")
+                                    .resizable()
+                                    .frame(width: 50, height: 50)
+                                    .clipShape(Circle())
+                            }
                         }
                     }
                 }
+                .padding(.horizontal)
+                .padding(.top, 72)
+                .padding(.bottom, 20)
             }
-            .padding(.horizontal)
-            .padding(.top, 72)
-            .padding(.bottom, 20)
+            .frame(maxWidth: .infinity)
+            .background(Color("ChatBackground"))
+            .onTapGesture {
+                focusedField = nil
+            }
+            .onChange(of: chat) {
+                withAnimation {
+                    if let lastMessage = chat.last {
+                        proxy.scrollTo(lastMessage.id, anchor: .bottom)
+                    }
+                }
+            }
         }
-        .frame(maxWidth: .infinity)
-        .background(Color("ChatBackground"))
     }
     
     var navigationArea: some View {
         // Navigation Area
         HStack {
+            Button(action: {
+                
+            }, label: {
+                Image(systemName: "line.3.horizontal")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            })
+            Spacer()
+                .frame(width: 5)
             Spacer()
             Text("Ai Diary")
                 .font(.system(size: 25, design: .monospaced))
                 .bold()
             Spacer()
+            NavigationLink(destination: CalendarView()) {
+                Image(systemName: "calendar")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+            }
         }
         .padding()
         .foregroundColor(Color("LaunchScreenBackGround"))
@@ -129,15 +163,17 @@ extension ChatView {
                 .background(Color("white_black"))
                 .clipShape(Capsule())
                 .foregroundColor(.primary)
+                .focused($focusedField, equals: .inputField)
             Button(action: {
-                let message = Message(id: UUID().uuidString, message: chatTextField, isUser: "user")
-                chat.append(message)
-                let chatText = chatTextField
-                chatTextField = ""
-                // 新しいメッセージの追加
-                messages.append(["role": "user", "content": chatText])
                 
-                if !chatText.isEmpty {
+                // キーボードをしまう
+                focusedField = nil
+                
+                if !chatTextField.isEmpty {
+                    let message = Message(id: UUID().uuidString, message: chatTextField, isUser: "user")
+                    chat.append(message)
+                    messages.append(["role": "user", "content": chatTextField])
+                    chatTextField = ""
                     
                     // ChatGPTのAPIにメッセージを送信
                     ChatBotAPIClient().sendMessageChatGPT(messages: messages) { result in
@@ -146,7 +182,6 @@ extension ChatView {
                             
                             // roleがuserのメッセージを抽出
                             let responseMessage = result.filter { $0["role"] == "assistant" }
-                            
                             
                             for massage in responseMessage {
                                 if let content = massage["content"] {
@@ -163,7 +198,7 @@ extension ChatView {
             }, label: {
                 Image(systemName: "paperplane.fill")
                     .resizable()
-                    .foregroundColor(Color("LaunchScreenBackGround"))
+                    .foregroundColor(chatTextField.isEmpty ? .gray.opacity(0.4) : Color("LaunchScreenBackGround"))
                     .frame(width: 20, height: 20)
                     .padding(.horizontal)
             })
